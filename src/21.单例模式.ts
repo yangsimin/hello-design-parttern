ca7b56ae2b5a28cb0e2d1fc599324066751113cf
yangsimin <yangsimin@users.noreply.github.com>
2024-09-20 19:42:37 +0800

    feat: add 单例模式

---
+++ src/21.单例模式.ts
@@ -0,0 +1,16 @@
+class Singleton {
+  private static instance: Singleton
+  private constructor() { }
+  public static getInstance(): Singleton {
+    if (!Singleton.instance) {
+      Singleton.instance = new Singleton()
+    }
+    return Singleton.instance
+  }
+}
+
+const s1 = Singleton.getInstance()
+const s2 = Singleton.getInstance()
+console.log(s1 === s2)
+
+export { }
